341b9132e4538cc5d7e9fca41f057d2f6a248295
johnmuthua <caveomuthama@gmail.com>
2022-09-16 02:10:06 +0300

    added permissions

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Post
@@ -11,6 +12,11 @@ class PostSerializer(serializers.ModelSerializer):
             'title',
             "body",
             "created_at",
-            "updated_at"
+            "updated_at",
         )
         model = Post
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username",)
