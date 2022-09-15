6d932d9bd1f3f2919f5c52cfab211e43868f4e13
johnmuthua <caveomuthama@gmail.com>
2022-09-16 00:28:54 +0300

    chore:created post serializer class

---
+++ posts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            "id",
+            "author",
+            "body",
+            "created_at",
+        )
+        model = Post
