3a08ce9bca50154d1d4272d069b8532a87b25f81
johnmuthua <caveomuthama@gmail.com>
2022-09-16 00:16:33 +0300

    feat:added rest_framework

---
+++ accounts/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserChangeForm
+    form = CustomUserChangeForm
+    list_display =[
+        "email",
+        "username",
+        "name",
+        "is_staff"
+    ]
+
+fieldsets = UserAdmin.fieldsets +((None, {'fields':('name',)}),)
+add_fieldsets = UserAdmin.add_fieldsets +((None, {'fields':('name',)}),)
+
+# Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
